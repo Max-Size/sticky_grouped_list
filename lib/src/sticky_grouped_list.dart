3062dfa6c08616780d0a7794d7e7c46ecefd28cd
Maxim Seleznev <maximuhkass14@gmail.com>
2025-04-13 20:30:18 +0300

    deleted unnecessary sorting

--- lib/src/sticky_grouped_list.dart
+++ lib/src/sticky_grouped_list.dart
@@ -229,7 +229,7 @@ class StickyGroupedListViewState<T, E>
 
   @override
   Widget build(BuildContext context) {
-    sortedElements = _sortElements();
+    sortedElements = widget.elements;
     var hiddenIndex = widget.reverse ? sortedElements.length * 2 - 1 : 0;
     _isSeparator = widget.reverse ? (int i) => i.isOdd : (int i) => i.isEven;
 
